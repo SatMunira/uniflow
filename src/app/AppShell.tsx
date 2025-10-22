5f5754f628f922b5ab90e0adc46eb1abad0465de
SatMunira <satanovamunira04@gmail.com>
2025-10-22 16:22:30 +0200

    feat(AppShell): restructure layout with Sidebar and BrandBadge components

--- src/app/AppShell.tsx
+++ src/app/AppShell.tsx
@@ -1,11 +1,19 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "@/components/layout/Navbar/Sidebar";
+import { BrandBadge } from "@/components/layout/Navbar/BrandBadge";
+import cls from "@/components/layout/Navbar/Sidebar.module.scss";
 
 export default function AppShell() {
   return (
     <div className="flex h-dvh bg-background text-foreground">
-      <Sidebar />
-      <main className="flex-1 min-w-0">
+      {/* левая рельса */}
+      <div className={cls.rail}>
+        <BrandBadge />
+        <Sidebar />
+      </div>
+
+      {/* контент скроллится тут */}
+      <main className="flex-1 min-w-0 overflow-auto grid place-items-center">
         <Outlet />
       </main>
     </div>
